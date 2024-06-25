0e161a3b9ecfc8aecbb49f29adb3965cb75230e3
SalomonKisters <141818828+SalomonKisters@users.noreply.github.com>
2024-06-25 23:51:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const { Alchemy, Network } = require("alchemy-sdk");
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
 const config = {
@@ -22,31 +21,40 @@ app.post('/nfts', async (req, res) => {
       return res.status(400).json({ error: 'Both wallet address and collection address are required' });
     }
 
-    // Get all NFTs for the collection
-    let collection_nft_ids = [];
-    const collectionResponse = await alchemy.nft.getNftsForContract(collectionAddress);
-    for (let nft of collectionResponse.nfts) {
-      collection_nft_ids.push(nft.contract.address);
-    }
+    const walletOpts = {
+      omitMetadata: true,
+      pageSize: 100,
+      tokenUriTimeoutInMs: 0,
+    };
 
-    // TODO: ALSO CHECK FOR TOKEN_ID
-
-    // Get all NFTs for the wallet
-    let wallet_nfts = [];
-    const walletResponse = await alchemy.nft.getNftsForOwner(walletAddress);
-    for (let nft of walletResponse.ownedNfts) {
-      wallet_nfts.push(nft.contract.address);
-    }
-
-    let wallet_has_collection = false;
-    for (let nft of wallet_nfts) {
-      if (collection_nft_ids.includes(nft)) {
-        wallet_has_collection = true;
-        break;
+    let matchingTokenIds = [];
+    for await (const nft of alchemy.nft.getNftsForOwnerIterator(walletAddress, walletOpts)) {
+      if (nft.contractAddress == collectionAddress) {
+        matchingTokenIds.push(nft.tokenId);
       }
     }
 
-    res.json({ wallet_has_collection });
+    if (matchingTokenIds.length == 0){ 
+      res.json( false )
+    };
+    
+
+    for (let tokenId of matchingTokenIds) {
+      const collectionOpts = {
+        omitMetadata: true,
+        pageSize: 1,
+        pageKey: tokenId
+      };
+
+      const collectionResponse = await alchemy.nft.getNftsForContract(collectionAddress, collectionOpts);
+
+      if (collectionResponse.nfts[0].tokenId == tokenId){ 
+        res.json( true )
+      };
+    }
+
+    res.json( false );
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while fetching the NFTs' });
