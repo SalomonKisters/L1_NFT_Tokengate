1467e416c64a4d924f61890b432f9e68b4caab01
SalomonKisters <141818828+SalomonKisters@users.noreply.github.com>
2024-06-25 22:34:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,24 +24,29 @@ app.post('/nfts', async (req, res) => {
 
     // Get all NFTs for the collection
     let collection_nft_ids = [];
-    const response = await alchemy.nft.getNftsForContract(collectionAddress);
-    for (let nft of response.nfts) {
-      collection_nft_ids.push(nft.tokenId);
+    const collectionResponse = await alchemy.nft.getNftsForContract(collectionAddress);
+    for (let nft of collectionResponse.nfts) {
+      collection_nft_ids.push(nft.contract.address);
     }
 
+    // TODO: ALSO CHECK FOR TOKEN_ID
+
     // Get all NFTs for the wallet
-    const wallet_nfts = await alchemy.nft.getNftsForOwner(walletAddress);
+    let wallet_nfts = [];
+    const walletResponse = await alchemy.nft.getNftsForOwner(walletAddress);
+    for (let nft of walletResponse.ownedNfts) {
+      wallet_nfts.push(nft.contract.address);
+    }
 
     let wallet_has_collection = false;
-    for (let nft of wallet_nfts.ownedNfts) {
-      if (nft.contract.address.toLowerCase() === collectionAddress.toLowerCase() && 
-          collection_nft_ids.includes(nft.tokenId)) {
+    for (let nft of wallet_nfts) {
+      if (collection_nft_ids.includes(nft)) {
         wallet_has_collection = true;
         break;
       }
     }
 
-    res.json({ "result": wallet_has_collection });
+    res.json({ wallet_has_collection });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while fetching the NFTs' });
